57aa8b2b8f71a9ff23109243eb7daaf1ce51ba4a
Al Chen <atc239@gmail.com>
2017-08-27 23:27:38 -0400

    fixed errors for compiling

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -25,26 +25,26 @@ contract FundMe {
 	//Create new campaign
 	function newCampaign(address _campaignAddress, uint _fundingGoal, uint _deadlineDate) returns (uint campaignID) {
 		campaignID = numCampaigns++;
-		Campaign c = campaigns[campaignID];
+		Campaign storage c = campaigns[campaignID];
 		c.campaignAddress = _campaignAddress;
 		c.fundingGoal = _fundingGoal;
 		c.deadlineDate = block.number + _deadlineDate;
 	}
 
 	//Funder funds a campaign
-	function fund(uint _campaignID) {
-		Campaign c = campaigns[campaignID];
-		Funder f = c.funders[c.numFunders++];
+	function fund(uint _campaignID) payable {
+		Campaign storage c = campaigns[_campaignID];
+		Funder storage f = c.funders[c.numFunders++];
 		f.funderAddress = msg.sender;
 		f.fundAmount = msg.value;
-		c.amount += f.fundAmount;
+		c.amountUSD += f.fundAmount;
 	}
 
 	//Check if campaign funding goal is reached (either funding goal met or deadline reached)
 	function checkGoalReached(uint _campaignID) returns (bool _goalReached) {
-		Campaign c = campaigns[_campaignID];
+		Campaign storage c = campaigns[_campaignID];
 		if (c.amountUSD >= c.fundingGoal) {
-			c.campaignAddress.send(c.amountUSD);
+			c.campaignAddress.transfer(c.amountUSD);
 			c.amountUSD = 0;
 			c.campaignAddress = 0;
 			c.fundingGoal = 0;
@@ -68,7 +68,7 @@ contract FundMe {
 			c.deadlineDate = 0;
 			c.numFunders = 0;
 			while (j <= n) {
-				c.funders[j].funderAddress.send(c.funders[j].fundAmount);
+				c.funders[j].funderAddress.transfer(c.funders[j].fundAmount);
 				c.funders[j].funderAddress = 0;
 				c.funders[j].fundAmount = 0;
 				j++;
